5019aef8476131b8ead94d3a50fbe75d895c3627
Duncan Skinner <duncanskinner94@gmail.com>
2019-10-17 14:18:38 +0100

    styling thermostat

--- jasmine-standalone-3.5.0/src/index.js
+++ jasmine-standalone-3.5.0/src/index.js
@@ -7,9 +7,17 @@ $(document).ready(function() {
     } else if (thermostat.viewEnergyUsage() === 'low-usage') {
       return $("#energy-usage-colour").removeClass("bg-warning").addClass("bg-success")
     } else {
-      return $("#energy-usage-colour").removeClass("bg-warning").addClass("bg-danger")
+      return $("energy-usage-colour").removeClass("bg-warning").addClass("bg-danger")
     }
   }
+
+  var date = new Date()
+  var time = date.toLocaleTimeString()
+  $("#time").text(time)
+
+  var day = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()
+  $("#date").text(day)
+  console.log(date)
 
   $('#temperature-current').text(thermostat.viewTemp())
   $("#power-saving-status").text(thermostat.getPowerSavingMode())
@@ -55,6 +63,20 @@ $(document).ready(function() {
   })
 
 
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city)
+    })
+  })
 
-
-})
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#temperature').text(data.main.temp)
+      $('#humidity').text(data.main.humidity)
+      $('#pressure').text(data.main.pressure)
+      $('#weather').text(data.weather[0].main)
+    })
+  }
